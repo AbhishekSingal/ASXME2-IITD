6592c519ab8c3b9726162baed886fe90a91d38ed
AbhishekSingal <abhisheksingal101@gmail.com>
2025-10-03 05:07:10 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="vcontainer">
+      <h1>Welcome !</h1>
+      <label>
+        Abhishek Singal - IIT Delhi , B.Tech (Industrial Engineering)
+      </label>
+    </div>
+  );
+}
+
+export default App;
